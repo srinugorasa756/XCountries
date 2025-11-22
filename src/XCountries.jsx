c137150e3011a9194ef72930f4a53e32337112a5
Srinivas <srinugorasa756@gmail.com>
2025-11-22 18:21:27 +0530

    displaying countries on mount

--- src/XCountries.jsx
+++ src/XCountries.jsx
@@ -1,16 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const API_ENDPOINT = "https://xcountries-backend.labs.crio.do/all"
+
 export default function XCountries(){
+    const [country, setCountry] = useState([]);
+    useEffect(()=>{
+        const fetchData = async () => {
+            try {
+                const apiData = await axios.get(API_ENDPOINT);
+                console.log({apiData});
+                setCountry(apiData.data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        }
+        fetchData();
+    }, [])
+
+    console.log({country});
+
     return(
-        <div>
-            <CountryCard />
+        <div style={{display:"flex", flexWrap:"wrap", justifyContent:"center", alignItems:"center", gap:"10px"}}>
+            {country.map(({name, flag, abbr}, i)=>(
+                <CountryCard key={i} name={name} flag={flag} abbr={abbr} />
+            ))}
         </div>
     )
 }
 
-function CountryCard(){
+function CountryCard({name, flag, abbr}){
     return(
         <div style={{display:"flex", flexDirection:"column", height:"200px", width:"200px", justifyContent:"center", alignItems:"center", border:"5px solid black", borderRadius:"10px"}}>
-            <img src="flag.png" alt="flag" />
-            <h3>Country Name</h3>
+            <img style={{height:"100px", width:"100px"}} src={flag} alt={`Flag of: ${abbr}`} />
+            <h3>{name}</h3>
         </div>
     )
 }
