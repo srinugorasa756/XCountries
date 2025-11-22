ea4a361cd22f7b9a1d69bd7386cffa430bc025f8
Srinivas <srinugorasa756@gmail.com>
2025-11-22 17:22:45 +0530

    changed border colour to black

--- src/XCountries.jsx
+++ src/XCountries.jsx
@@ -8,7 +8,7 @@ export default function XCountries(){
 
 function CountryCard(){
     return(
-        <div style={{display:"flex", flexDirection:"column", height:"200px", width:"200px", justifyContent:"center", alignItems:"center", border:"5px solid white", borderRadius:"10px"}}>
+        <div style={{display:"flex", flexDirection:"column", height:"200px", width:"200px", justifyContent:"center", alignItems:"center", border:"5px solid black", borderRadius:"10px"}}>
             <img src="flag.png" alt="flag" />
             <h3>Country Name</h3>
         </div>
