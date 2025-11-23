2bcaf15f0948aeebca80ba64ec56ded471cb39c6
Srinivas <srinugorasa756@gmail.com>
2025-11-23 15:50:30 +0530

    removed timer

--- src/XCountries.jsx
+++ src/XCountries.jsx
@@ -1,42 +1,28 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+
 import CircularProgress from "@mui/material/CircularProgress";
 
 const API_ENDPOINT = "https://xcountries-backend.labs.crio.do/all"
 
 export default function XCountries(){
     const [country, setCountry] = useState([]);
-    const [loading, setLoading] = useState(true);
     useEffect(()=>{
-        const timer = setTimeout(()=>{
             const fetchData = async () => {
             try {
-                const apiData = await axios.get(API_ENDPOINT);
-                console.log({apiData});
-                setCountry(apiData.data);
+                const apiData = await fetch(API_ENDPOINT);
+                // console.log({apiData});
+                // setCountry(apiData.data);
+                const initData = await apiData.json();
+                setCountry(initData);
             } catch (error) {
-                console.error("Error fetching data: ", error);
-                setLoading(false);
+                console.error("Error fetching data:", error);
             }
-             finally{
-                setLoading(false);
-            }
+
         }
         fetchData();
-        return ()=>clearTimeout(timer);
-        }, 2000)
     }, [])
 
     console.log({country});
-
-    if (loading) {
-        return (
-            <div style={{ textAlign: "center", marginTop: "40px" }}>
-            <CircularProgress size={60} thickness={4} />
-            <h3>Loading countries...</h3>
-            </div>
-        );
-}
 
     return(
         <div style={{display:"flex", flexWrap:"wrap", justifyContent:"center", alignItems:"center", gap:"10px"}}>
